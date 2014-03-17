da829e953ac6027c695f1a3548232c9cd1415b25
FiipJany <filip.jany@gmail.com>
2014-03-17 21:43:21 +0100

    Added database code
    Testowane na randomowych danych bez bledow (przed i po dodaniu rekordow)

--- flavority/flavority/models.py
+++ flavority/flavority/models.py
@@ -9,6 +9,21 @@ from flavority.auth.mixins import UserMixin
 
 db = app.db
 
+#Associations aka Logic tables (many-to-many connections)
+#DEL -> te takie inne niebieskie tabelki w uml'u, dla mnie to one sa niebieskie ale pewnie jakos inaczej ten kolor sie zwie, whatever
+#Table will connect Ingredients with Recipes
+ingredient_assignment = db.Table('ingredient_assignment',
+                                 db.Column('recipe', db.Integer, db.ForeignKey('Recipe.id')),
+                                 db.Column('ingr', db.Integer, db.ForeignKey('Ingredient.id')),
+                                 db.Column('amount', db.Integer))
+tag_assignment = db.Table('tag_assignment',
+                          db.Column('recipe', db.Integer, db.ForeignKey('Recipe.id')),
+                          db.Column('tag', db.Integer, db.ForeignKey('Tag.id')))
+favour_recipes = db.Table('favour_recipes',
+                          db.Column('user', db.Integer, db.ForeignKey('User.id')),
+                          db.Column('recipe', db.Integer, db.ForeignKey('Recipe.id')))
+#End of associations declaration
+
 class User(db.Model, UserMixin):
 
     # should be sufficient
@@ -41,6 +56,7 @@ class User(db.Model, UserMixin):
     salt  = db.Column(db.String(PASSWORD_LENGTH), nullable = False)
     password = db.Column(db.String(PASSWORD_LENGTH), nullable = False)
     type = db.Column(db.Enum(*tuple(USER_TYPES.values()), name = USER_TYPE_ENUM_NAME), default = USER_TYPES[USER_TYPE_COMMON])
+    favourites = db.relationship('Recipe', secondary=favour_recipes)
 
     @staticmethod
     def gen_salt(length = HASH_SIZE):
@@ -71,3 +87,157 @@ class User(db.Model, UserMixin):
     def get_id(self):
         return self.id
 
+    def __repr__(self):
+        return '<User: %r, with password: %r and email: %r>' % (self.id,  self.password, self.email)
+#End of 'User' class declaration
+
+
+#Class represents the recipe's object with name 'Recipe'
+#Arg: db.Model - model from SQLAlchemy database
+class Recipe(db.Model):
+    
+    DESCRIPTION_LENGTH = 120
+    
+    __tablename__ = 'Recipe'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    dish_name = db.Column(db.String(DESCRIPTION_LENGTH))
+    author_id = db.Column(db.Integer, db.ForeignKey('User.id'))
+    author = db.relationship('User', backref=db.backref('recipes', lazy='dynamic'))
+    creation_date = db.Column(db.Date)
+    preparation_time = db.Column(db.SmallInteger)
+    photo = db.Column(db.BLOB)
+    recipe_text = db.Column(db.Text)
+    tasteMark = db.Column(db.Float)
+    difficultyMark = db.Column(db.Float)
+    rank = db.Column(db.Float)
+    eventToAdminControl = db.Column(db.Boolean)
+    portions = db.Column(db.SmallInteger)
+    ingredients = db.relationship('Ingredient', secondary=ingredient_assignment)
+    tags = db.relationship('Tag', secondary=tag_assignment)
+    
+    def __init__(self, dish_name, creation_date, preparation_time, recipe_text, portions, author):
+        self.dish_name = dish_name
+        if creation_date is None:
+            self.creation_date = datetime.now()
+        self.preparation_time = preparation_time
+        self.recipe_text = recipe_text
+        self.portions = portions
+        self.author = author
+    
+    def __repr__(self):
+        return '<Recipe name : %r, posted by : %r>' % (self.dish_name, self.author_id)
+#End of 'Recipe' class declaration
+
+
+#Class represents the Comment's object with name 'Comment'
+#Arg: db.Model - model from SQLAlchemy database
+class Comment(db.Model):
+    
+    COMMENT_TITLE_LENGTH = 120
+    
+    __tablename__ = 'Comment'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String(COMMENT_TITLE_LENGTH))
+    text = db.Column(db.Text)
+    author_id = db.Column(db.Integer, db.ForeignKey('User.id'))
+    author = db.relationship('User', backref=db.backref('comments', lazy='dynamic'))        #DELmany to one z comment do usera
+    recipe_id = db.Column(db.Integer, db.ForeignKey('Recipe.id'))
+    recipe = db.relationship('Recipe', backref=db.backref('recipes', lazy='dynamic'))       #DElmany to one z comment do recipe
+    
+    def __init__(self, title, text, author_id, recipe_id):
+        self.title = title
+        self.text = text
+        self.author_id = author_id
+        self.recipe_id = recipe_id
+    
+    def __repr__(self):
+        return '<Comment: %r, with text: %r>' % (self.title, self.text)
+#End of 'Comment' class declaration
+
+
+#Class represents the Rate object with name 'Rate'
+#Arg: db.Model - model from SQLAlchemy database
+class Rate(db.Model):
+    __tablename__ = 'Rate'
+    id = db.Column(db.Integer, primary_key=True)
+    author_id = db.Column(db.Integer, db.ForeignKey('User.id'))
+    author = db.relationship('User', backref=db.backref('rates', lazy='dynamic'))       #DELmany to one z rate do usera
+    recipe_id = db.Column(db.Integer, db.ForeignKey('Recipe.id'))
+    recipe = db.relationship('Recipe', backref=db.backref('rates', lazy='dynamic'))       #DELmany to one z comment do recipe
+    taste_rate = db.Column(db.SmallInteger)
+    difficulty_rate = db.Column(db.SmallInteger)
+    
+    def __init__(self, taste, difficulty, author, recipe):
+        self.taste_rate = taste
+        self.difficulty_rate = difficulty
+        self.author = author
+        self.recipe = recipe
+    
+    def __repr__(self):
+        return '<Rate from userID : %r, to recipeID : %r, taste : %r, difficulty : %r>' % (self.author_id, self.recipe_id, self.taste_rate, self.difficulty_rate)
+#End of 'Rate' class declaration
+
+
+#Class represents the Ingredient's object with name 'Ingredient'
+#Arg: db.Model - model from SQLAlchemy database
+class Ingredient(db.Model):
+    
+    INGREDIENT_NAME_LENGTH = 100
+    
+    __tablename__ = 'Ingredient'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(INGREDIENT_NAME_LENGTH), unique=True)
+    unit_id = db.Column(db.Integer, db.ForeignKey('Unit.id'))
+    unit = db.relationship('Unit', backref=db.backref('units', lazy='dynamic'))
+    
+    def __init__(self, name):
+        self.name = name
+    
+    def __repr__(self):
+        return '<Ingredient\'s name : %r>' % self.name
+#End of 'Ingredient' class declaration
+
+
+#Class represents the Unit's object with name 'Unit'
+#Arg: db.Model - model from SQLAlchemy database
+class Unit(db.Model):
+    
+    UNIT_NAME_LENGTH = 40
+    
+    __tablename__ = 'Unit'
+    id = db.Column(db.Integer, primary_key=True)
+    unit_name = db.Column(db.String(UNIT_NAME_LENGTH), unique=True) #DELtak mi sie wydaje :P
+    unit_value = db.Column(db.Float)
+    other_id = db.Column(db.Integer, db.ForeignKey('Unit.id'))      #DELnie do konca zalapalem czemu, ale to musi tu zostac
+    others = db.relationship('Unit', remote_side=[id])              #DELwedle wszelkich znakow w internetach ta relacja many to one powinna chodzic
+    #DELsee -> Adjacency List Relationships at SQLAlchemy
+    def __init__(self, unit_name, unit_value, others):
+        self.unit_name = unit_name
+        self.unit_value = unit_value
+        self.others = others
+    
+    def __repr__(self):
+        return '<Unit : %r, with value : %r>' % (self.unit_name, self.unit_value)
+#End of 'Unit' class declaration
+
+
+#Class represents the Tag's object with name 'Tag'
+#Arg: db.Model - model from SQLAlchemy database
+class Tag(db.Model):
+    
+    TAG_NAME_LENGTH = 40
+    TAG_TYPE_LENGTH = 39
+    
+    __tablename__ = 'Tag'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(TAG_NAME_LENGTH), unique=True)
+    type = db.Column(db.String(TAG_TYPE_LENGTH))
+    
+    def __init__(self, name, type):
+        self.name = name
+        self.type = type
+    
+    def __repr__(self):
+        return '<Tag name : %r and type : %r>' % (self.name, self.type)
+#End of 'Tag' class declaration
+#EOF
