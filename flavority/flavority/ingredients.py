cf43da53a61ba80e3d2fcec38c158be78906bfdb
Joanna <joanna.cislo@gmail.com>
2014-05-28 20:19:55 +0200

    Added possibility of retrieving list of ingredients.

---
+++ flavority/flavority/ingredients.py
@@ -0,0 +1,22 @@
+
+from flask.ext.restful import Resource, reqparse
+from sqlalchemy import desc, func
+
+from . import app
+from .models import Ingredient
+from .util import ViewPager
+
+
+class IngredientsResource(Resource):
+
+    GET_ITEMS_PER_PAGE = 30
+
+    def options(self):
+        return None
+
+    def get(self):
+        """
+        Returns a list of all ingredients in database ordered by name.
+        """
+        return [{'id': ingr.id, 'name': ingr.name} for ingr in Ingredient.query.order_by(Ingredient.name).all()]
+
